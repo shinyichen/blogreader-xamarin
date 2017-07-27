f140e9ac066f400d13ded27ac496aee3adb8ed5d
Shinyi Chen <jchen@isi.edu>
2017-07-26 17:24:52 -0700

    Initial commit

---
+++ shinyichen/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Collections.Generic;
+
+using Android.Util;
+using WordPressPCL;
+using WordPressRestApiStandard;
+using WordPressRestApiStandard.Models;
+using Android.Support.V7.Widget;
+
+namespace shinyichen
+{
+    [Activity(Label = "android", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+		private string url = "http://shinyichen.com/blog/wp-json/wp/v2/";
+		const string username = "android";
+		const string password = "hjTxS2W5%CRD(SusyrhigD1e";
+        private List<Post> posts;
+
+        private RecyclerView postListView;
+        private RecyclerView.LayoutManager layoutManager;
+        private PostListAdapter postListAdapter;
+
+
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // load posts
+			WordPressApiClient client = new WordPressApiClient(url);
+
+			posts = await client.GetPosts(new WordPressRestApiStandard.QueryModel.PostsQuery() { PerPage = 8 });
+
+            postListView = FindViewById<RecyclerView>(Resource.Id.postListView);
+            layoutManager = new LinearLayoutManager(this);
+            postListView.SetLayoutManager(layoutManager);
+
+            postListAdapter = new PostListAdapter(posts, client);
+            postListView.SetAdapter(postListAdapter);
+
+        }
+
+    }
+}
+
---
+++ shinyichen/PostListAdapter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Support.V7.Widget;
+using Android.Util;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using WordPressRestApiStandard;
+using WordPressRestApiStandard.Models;
+using WordPressRestApiStandard.QueryModel;
+
+namespace shinyichen
+{
+    public class PostListAdapter : RecyclerView.Adapter
+    {
+        private WordPressApiClient wpClient;
+        private List<Post> posts;
+
+        public PostListAdapter(List<Post> posts, WordPressApiClient wpClient)
+        {
+            this.posts = posts;
+            this.wpClient = wpClient;
+        }
+
+        public override int ItemCount {
+            get {
+                return posts.Count;
+            }
+        }
+
+        public override async void OnBindViewHolder(RecyclerView.ViewHolder holder, int position)
+        {
+            PostViewHolder vh = holder as PostViewHolder;
+            Post post = posts[position];
+            vh.PostTitleView.Text = post.Title.Rendered;
+            if (post.FeaturedMedia > 0)
+            {
+                Media media = await wpClient.GetMedia(new MediaQuery(), post.FeaturedMedia);
+                string url = media.MediaDetails.Sizes.Thumbnail.SourceUrl;
+                Log.Info("adapter", url);
+                // TODO set image view
+            }
+        }
+
+        public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
+        {
+            View view = LayoutInflater.From(parent.Context).Inflate(Resource.Layout.PostCard, parent, false);
+            PostViewHolder vh = new PostViewHolder(view);
+            return vh;
+        }
+    }
+
+    public class PostViewHolder : RecyclerView.ViewHolder 
+    {
+        public ImageView PostImageView { get; private set; }
+        public TextView PostTitleView { get; private set; }
+
+        public PostViewHolder(View view) : base(view)
+        {
+            PostImageView = view.FindViewById<ImageView>(Resource.Id.postImageView);
+            PostTitleView = view.FindViewById<TextView>(Resource.Id.postTitleView);
+        }
+    }
+}
